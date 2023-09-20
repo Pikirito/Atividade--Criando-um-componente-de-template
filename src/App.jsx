2ffa3b3eabf868f4675b9849dd9397cd36543744
Eduardo <eduaraujp760@gmail.com>
2023-09-20 13:05:23 -0400

    Atividade resolvida

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { DefaultTemplate } from "./components/DefaultTemplate";
+
+function App() {
+  return (
+    <>
+      <DefaultTemplate>
+        <h1>Eu sou uma children</h1>
+      </DefaultTemplate>
+    </>
+  );
+}
+
+export default App;
